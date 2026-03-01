341e0fc91d6e4b27c6e52d461657d38b56ecbeb5
Benny Maliti <malitiben@gmail.com>
2026-03-01 22:43:03 +0000

    fix: add API Gateway CloudWatch account role, remove access_log_settings from stage

--- terraform/main.tf
+++ terraform/main.tf
@@ -182,3 +182,30 @@ resource "aws_cloudwatch_dashboard" "main" {
     ]
   })
 }
+
+# --- API Gateway Account CloudWatch Logs Role ---
+resource "aws_iam_role" "api_gateway_cloudwatch" {
+  name = "${local.name_prefix}-apigw-cloudwatch"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch" {
+  role       = aws_iam_role.api_gateway_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_api_gateway_account" "main" {
+  cloudwatch_role_arn = aws_iam_role.api_gateway_cloudwatch.arn
+}
